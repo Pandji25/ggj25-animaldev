167b540dd94f25f81bb392886000b0f31e0e3795
Pandji25 <mnzulkarnaien@gmail.com>
2025-01-27 09:28:41 +0700

    Removed escape key button for web export.

--- stage.gd
+++ stage.gd
@@ -11,10 +11,6 @@ func _ready() -> void:
 	level_progression.play(&"level_progress")
 
 func _input(event: InputEvent) -> void:
-	# Will probably be changed into pause.
-	if event is InputEventKey:
-		if event.is_action_pressed("ui_cancel"):
-			get_tree().quit()
 		if event.is_action_pressed("Retry") and playerlive != 1:
 			get_tree().reload_current_scene()
 			playerlive = 1
